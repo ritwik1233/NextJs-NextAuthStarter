d84cbc67e30b4deb35048a127e87ea597937dd31
Ritwik Sinha <ritwiksinha@Ritwiks-MacBook-Air.local>
2024-09-25 19:42:34 +0530

    translation fixes

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -56,10 +56,6 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
-  pages: {
-    signIn: "/auth/login", // Custom sign-in page
-    error: "/auth/error", // Optional custom error page
-  },
   session: {
     strategy: "jwt",
   },
